d8dc400cb8c7102b6883b097f27b757057b45513
Anton <kondratyuk.anton.ig@gmail.com>
2018-11-05 11:30:36 +0300

    update her

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log(`Server listening on 3000 port`);
+app.listen(port, () => {
+  console.log(`Server listening on 8080 port`);
 });
